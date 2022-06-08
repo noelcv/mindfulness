0315390f64291d3dacae62db08da5f46344ce525
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-08 18:58:03 +0200

    add: Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
+import React from 'react';
+
 import Header from './components/appLevel/Header/Header';
 import SideBar from './components/appLevel/SideBar/SideBar';
 import Dashboard from './components/appLevel/Dashboard/Dashboard';
 
+import { Counter } from './features/counter/Counter';
 import './App.css';
 
-const App = () => {
-  return (
+function App() {
+  return ( 
     <div className="App">
-      <Header/>
-      <div className="app-holder">
-      <SideBar/>
-      <Dashboard/>
-    </div>
+        <Header/>
+        <div className="app-holder">
+        <SideBar/>
+        <Dashboard/>
+      </div>
     </div>
   );
 }

f06e27370b3b94142a2c99bcf4e0aa54d65cdd7e
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-08 22:17:49 +0200

    style: sidebar-links

--- client/src/components/appLevel/SideBar/SideBar.jsx
+++ client/src/components/appLevel/SideBar/SideBar.jsx
@@ -10,10 +10,10 @@ import SettingsBtn from './SettingsBtn/SettingsBtn';
 const SideBar = () => {
   return (
     <div className="sidebar-container">
-    <Link to="/profile"><ProfileBtn/></Link>
-    <Link to="/events"><EventsBtn/></Link>
-    <Link to="/classroom"><ClassroomBtn/></Link>
-    <Link to="/settings"><SettingsBtn/></Link>
+    <Link to="/profile" className="sidebar-link"><ProfileBtn/></Link>
+    <Link to="/events" className="sidebar-link"><EventsBtn/></Link>
+    <Link to="/classroom" className="sidebar-link"><ClassroomBtn/></Link>
+    <Link to="/settings" className="sidebar-link"><SettingsBtn/></Link>
     </div>
   )
 }

a07b72fa552123020d12811d0c793477b0d50b20
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-08 16:58:01 +0200

    chore: Dashboard.jsx basic structure

---
+++ client/src/components/appLevel/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Dashboard.css';
+import ClassBoard from '../../Classroom/ClassBoard/ClassBoard';
+
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+    <ClassBoard/>
+    </div>
+  )
+}
+
+export default Dashboard;

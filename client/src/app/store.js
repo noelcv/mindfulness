83634bee3397b9124a18e72389672e29f1d95a21
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-08 21:53:41 +0200

    fix: redux configureStore

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from 'redux';
+import { reducers } from '../reducer';
+// import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const store = configureStore(reducers);
+
+
+
+
+
+
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });

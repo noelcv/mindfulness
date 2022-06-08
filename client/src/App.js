fa5c84186ac989e91e75045dced383eefb939a90
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-08 19:08:18 +0200

    redux: wrap up the app with Provider

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,21 @@ import React from 'react';
 import Header from './components/appLevel/Header/Header';
 import SideBar from './components/appLevel/SideBar/SideBar';
 import Dashboard from './components/appLevel/Dashboard/Dashboard';
-
+import { Provider } from 'react-redux';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-  return ( 
-    <div className="App">
-        <Header/>
+  return (
+    <Provider>
+      <div className="App">
+        <Header />
         <div className="app-holder">
-        <SideBar/>
-        <Dashboard/>
+          <SideBar />
+          <Dashboard />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 

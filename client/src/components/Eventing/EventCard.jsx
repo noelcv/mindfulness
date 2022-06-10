dfcd65cd163e8a1a2e093d6f87ff34e0681e57b9
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-10 17:41:12 +0200

    chore: EventCard component

--- client/src/components/Eventing/EventCard.jsx
+++ client/src/components/Eventing/EventCard.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import moment from 'moment';
 
-const EventCard = () => {
-  return;
-  <div>EventCard</div>;
+const EventCard = ({event}) => {
+  return (
+    <>
+      <div className="event-container">
+        <div className="event-date-container">
+          <h3 className="event-date" >{moment(event.date).format(`Do MMM`)}</h3>
+      
+        </div>
+        <div className="event-content-container">
+          <h3 className="event-title">{event.title}</h3>
+          <div className="event-details-container">
+          <p className="event-detail event-time">{moment(event.date).format('hh:mm a - MMMM Do, YYYY')}</p>
+          <p className="event-detail event-location">{event.location}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default EventCard;

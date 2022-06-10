35bfc9acf443f8125aa5e0a867333c794abd7559
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-10 18:16:13 +0200

    chore: import EventForm to EventsPage

--- client/src/components/pages/EventsPage.jsx
+++ client/src/components/pages/EventsPage.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../appLevel/Header/Header';
 import SideBar from '../appLevel/SideBar/SideBar';
 import EventList from '../Eventing/EventList';
 import EventForm from '../Eventing/EventForm';
 import './CommonPageStyles.css';
 
-
 const EventsPage = () => {
-  
-  
+  //HOOKS
+
+  const [events, setEvents] = useState([]);
+
   return (
     <div className="app">
       <Header />
       <div className="app-holder">
         <SideBar />
         <div className="dashboard-container">
-        <h2 className="page-title">Events</h2>
-        
+          <h2 className="page-title">Events</h2>
+          <EventForm setEvents={setEvents} />
         </div>
       </div>
     </div>

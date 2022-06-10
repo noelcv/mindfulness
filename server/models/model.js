39b05b604df8f88146565bea6c4a489eaa69b96c
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-10 17:08:16 +0200

    chore: adjust Schema / works with Postman

--- server/models/model.js
+++ server/models/model.js
@@ -5,11 +5,10 @@ const Schema = mongoose.Schema;
 //define Schema
 const eventSchema = new Schema({
   id: {type: String},
-  classroomId: {type: String}, //only generated for online events
-  classroomLocation: {type: String}, //only for offline events 
   title: {type: String, required: true},
   date: {type: String, required: true},
-  venue: {type: String, required: true}
+  classroomId: {type: String}, //only generated for online events
+  location: {type: String}, //only for offline events 
 });
 
 const EventModel = mongoose.model('EventModel', eventSchema);

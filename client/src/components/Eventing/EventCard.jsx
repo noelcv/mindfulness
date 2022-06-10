2770512fcce409db874afb95005c4abedd6ab239
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-10 22:24:58 +0200

    feat: implement useNavigate hook to join classroom

--- client/src/components/Eventing/EventCard.jsx
+++ client/src/components/Eventing/EventCard.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import moment from 'moment';
-import './EventForm.css'; 
+import { useNavigate } from 'react-router-dom';
+import './EventForm.css';
 
-const EventCard = ({event}) => {
+
+const EventCard = ({ event }) => {
+  let goTo = useNavigate();
+  
+  const goToEvent = () => {
+    let eventPath = '../classroom';
+    goTo(eventPath, {replace: true});
+  }
+  
+  
   return (
     <>
       <div className="event-container">
         <div className="event-date-container">
-          <h3 className="event-date" >{moment(event.date).format(`Do MMM`)}</h3>
-      
+          <h3 className="event-date">{moment(event.date).format(`Do MMM`)}</h3>
         </div>
         <div className="event-content-container">
-          <h3 className="event-title">{event.title}</h3>
-          <div className="event-details-container">
-          <p className="event-detail event-time">{moment(event.date).format('hh:mm a - MMMM Do, YYYY')}</p>
-          <p className="event-detail event-location">{event.location}</p>
+          <div className="event-content-wrapper">
+            <div className="event-title-wrapper">
+              <h3 className="event-title">{event.title}</h3>
+            </div>
+            <div className="event-details-container">
+              <p className="event-detail event-time">
+                {moment(event.date).format('hh:mm a - MMMM Do, YYYY')}
+              </p>
+              <p className="event-detail event-location">{event.location}</p>
+            </div>
+          </div>
+          <div className="go-to-event-container">
+            <button className="go-to-event-btn submit-btn" onClick={goToEvent}>Join</button>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 };
 
 export default EventCard;

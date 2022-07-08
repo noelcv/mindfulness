35b664947f6ba8c875b9f81ba0265844847a4049
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-08 20:53:09 +0200

    MongoDB Atlas setup

--- server/config.js
+++ server/config.js
@@ -4,6 +4,5 @@ dotenv.config();
 module.exports = {
   clientPath: __dirname + '../client',
   PORT: process.env.PORT,
-  DB_URL: process.env.DB_URL,
-  DB_NAME: process.env.DB_NAME
+  URI: process.env.MONGODB_URI
 }
--- server/models/index.js
+++ server/models/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const {DB_URL, DB_NAME} = require('../config');
+const {URI} = require('../config');
 
 const mongoose = require('mongoose');
 
 (async () => {
   try {
-    await mongoose.connect(`${DB_URL}/${DB_NAME}`, {
+    await mongoose.connect(`${URI}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log(
-      `Mongoose connection established at ${DB_URL}/${DB_NAME}`
+      `Mongoose connection established`
     );
   } catch (error) {
     console.log(error);

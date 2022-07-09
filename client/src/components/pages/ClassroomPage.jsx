e11dcda3cbe656e842aac835a581622691a10c7e
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-09 13:27:52 +0200

    switch to DEV

--- client/src/components/pages/ClassroomPage.jsx
+++ client/src/components/pages/ClassroomPage.jsx
@@ -32,7 +32,7 @@ const Video = (props) => {
 
 const DEV = "http://localhost:3002";
 const PROD = 'https://mindfulnessp2p.herokuapp.com';
-const BASE_URL = DEV;
+const BASE_URL = PROD;
 
 const ClassroomPage = () => {
   //HOOKS for classroom state management

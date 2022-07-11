fb5c1d69407939df6589c222b132d3229df8d968
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-11 20:43:55 +0200

    fix: img src referrerPolicy

--- client/src/components/pages/ProfilePage.jsx
+++ client/src/components/pages/ProfilePage.jsx
@@ -6,35 +6,37 @@ import buymeacoffee from '../../assets/buymeacoffee.svg'
 
 import { UserAuth } from '../../AuthContext/AuthContext';
 import SignInPage from './SignInPage';
-import SignInBtn from '../appLevel/SideBar/SigninBtn/SigninBtn';
+
 
 
 const ProfilePage = () => {
   
   const { user } = UserAuth();
-  const profilePic = user?.photoURL;
-  
+    
   return (
     <div className="app">
       <Header />
       <div className="app-holder">
         <SideBar />
-      { user?.displayName ? 
+      { user?.displayName && user?.photoURL ? 
         <div className='dashboard-container profile-container'>
         <div className='profile-inner-container'>
           <div className='profile-detail name-card'>
-            <img src={profilePic} className="profile-pic" alt="profilePic"/>
+            <img src={user?.photoURL} className="profile-pic" referrerPolicy="no-referrer"  alt="profilePic" />
             <h2 className="display-name">{user?.displayName}</h2>
           </div>
-          
+          <div className='profile-detail email-card'>
+            <h4>Email</h4> 
+            <p className="display-email">{user?.email}</p>
+          </div>
           <div className='profile-detail location-card'>
             <h4>I am based in</h4>
-            <h2  className="display-location" contentEditable="false">Luxembourg</h2>
+            <h2  className="display-location">Luxembourg</h2>
           </div>
           
           <div className='profile-detail job-card'>
             <h4>I am a </h4>
-            <h2 className="display-job" contentEditable="false">Yoga Teacher</h2>
+            <h2 className="display-job">Yoga Teacher</h2>
           </div>
           
           <div className='profile-detail expertise-card'>
@@ -48,7 +50,7 @@ const ProfilePage = () => {
           
           <div className='profile-detail payment-card'>
             <img src={buymeacoffee} alt="buy-me-a-coffee" width="180px" height="75px"></img>
-            <a href="buymeacoffee.com/" className="buy-me-a-coffee-link" contentEditable="false">http://buymeacofee.com/xpto</a>
+            <a href="buymeacoffee.com/" className="buy-me-a-coffee-link">http://buymeacofee.com/xpto</a>
           </div>
           </div>
         </div>
--- client/src/components/pages/SettingsPage.jsx
+++ client/src/components/pages/SettingsPage.jsx
@@ -2,50 +2,49 @@ import React from 'react';
 import Header from '../appLevel/Header/Header';
 import SideBar from '../appLevel/SideBar/SideBar';
 import { UserAuth } from '../../AuthContext/AuthContext';
+import SignInPage from './SignInPage';
 import linkedin from '../../assets/linkedin.svg'
 import './SettingsPage.css';
 
 const SettingsPage = () => {
   
-  const { user } = UserAuth;
+  const { user } = UserAuth();
   
   return (
     <div className="app">
       <Header />
       <div className="app-holder">
         <SideBar />
+        { user?.displayName ? 
         <div className='dashboard-container profile-container'>
         <div className='profile-inner-container'>
-          <div className='profile-detail name-card'>
+          <div className='profile-detail email-card'>
             <h4>Email</h4> 
-            <p className="display-email" contentEditable="true">{user?.displayName} noelguiavieira@gmail.com</p>
+            <p className="display-email">{user?.email}</p>
           </div>
           
-          <div className='profile-detail location-card'>
+          <div className='profile-detail phone-card'>
             <h4>Phone Number</h4>
-            <p className="display-phone-number" contentEditable="true"> +352 621 420 678</p>
+            <p className="display-phone-number">{user?.phoneNumber}</p>
           </div>
           
           <div className='profile-detail job-card'>
             <h4>Billing Address</h4>
-            <p className="display-job" contentEditable="true">Yoga Teacher</p>
+            <p className="display-job"></p>
           </div>
           
           <div className='profile-detail expertise-card'>
             <h4>VAT</h4>
-            <ul className="expertise-list" contentEditable="true">
-              <li>AcroYoga</li> 
-              <li>Kundalini</li>
-              <li>Sound Baths</li>
-            </ul>
           </div>
           
           <div className='profile-detail payment-card'>
             <img src={linkedin} alt="linkedin" width="100px" height="100px"/>
-            <a href="linkedin.com/" className="linkedin-link" contentEditable="true">https://www.noelvieira.com</a>
+            <a href="linkedin.com/" className="linkedin-link">https://www.noelvieira.com</a>
           </div>
           </div>
         </div>
+        : <SignInPage /> 
+      }
       </div>
     </div>
   );

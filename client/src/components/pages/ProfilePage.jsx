5807a325d8c81179ac922c5f308f7eb0f2f6ac35
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-11 16:42:46 +0200

    css: restyled Profile Page + contentEditable

--- client/src/components/pages/ProfilePage.jsx
+++ client/src/components/pages/ProfilePage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from '../appLevel/Header/Header';
 import SideBar from '../appLevel/SideBar/SideBar';
-import './CommonPageStyles.css';
+import './ProfilePage.css';
+import buymeacoffee from '../../assets/buymeacoffee.svg'
 
 import { UserAuth } from '../../AuthContext/AuthContext';
 import SignOutBtn from '../appLevel/SideBar/SignOutBtn/SignOutBtn';
@@ -18,18 +19,34 @@ const ProfilePage = () => {
         <SideBar />
         <div className='dashboard-container profile-container'>
         <div className='profile-inner-container'>
-          <h4>My name is</h4>
+          <div className='profile-detail name-card'>
+            <h4>My name is</h4> 
+            <h2 className="display-name" contentEditable="true">{user?.displayName} Noel</h2>
+          </div>
           
-          <h2 className="profile-title">{user?.displayName}</h2>
+          <div className='profile-detail location-card'>
+            <h4>I am based in</h4>
+            <h2  className="display-location" contentEditable="true">Luxembourg</h2>
+          </div>
           
-          <h4>I am based in</h4>
-          <h2  className="profile-title">Luxembourg</h2>
+          <div className='profile-detail job-card'>
+            <h4>I am a </h4>
+            <h2 className="display-job" contentEditable="true">Yoga Teacher</h2>
+          </div>
           
-          <h4>I am a </h4>
-          <h2 className="profile-title">Yoga Teacher</h2>
+          <div className='profile-detail expertise-card'>
+            <h4>With an Expertise in</h4>
+            <ul className="expertise-list" contentEditable="true">
+              <li>AcroYoga</li> 
+              <li>Kundalini</li>
+              <li>Sound Baths</li>
+            </ul>
+          </div>
           
-          <h4>With an Expertise in</h4>
-          <h2 className="profile-title">AcroYoga, Kundalini and Sound Baths</h2>
+          <div className='profile-detail payment-card'>
+            <img src={buymeacoffee} alt="buy-me-a-coffee" width="180px" height="75px"></img>
+            <a href="buymeacoffee.com/" className="buy-me-a-coffee-link" contentEditable="true">http://buymeacofee.com/xpto</a>
+          </div>
           </div>
         </div>
       </div>

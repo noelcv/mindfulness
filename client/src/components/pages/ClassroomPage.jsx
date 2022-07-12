d3b02e4eb88317ad9c2ed1d64794aa8d7e2d6562
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-12 19:55:15 +0200

    audio: remove muted attribute

--- client/src/components/pages/ClassroomPage.jsx
+++ client/src/components/pages/ClassroomPage.jsx
@@ -4,8 +4,8 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import Peer from 'simple-peer';
 import io from 'socket.io-client';
 import { BACKEND_CONNECTION } from '../../utils/envSwitch';
+import { avSettings } from '../../utils/avSettings';
 import Header from '../appLevel/Header/Header';
-import SideBar from '../appLevel/SideBar/SideBar';
 import './ClassroomPage.css';
 import './CommonPageStyles.css';
 
@@ -24,7 +24,6 @@ const Video = (props) => {
     <>
       <video
         playsInline
-        muted
         autoPlay
         ref={ref}
         className="videoplayer-container"
@@ -48,19 +47,13 @@ const ClassroomPage = () => {
   const currentPath = useLocation();
   const roomId = currentPath.pathname.split('/').pop();
 
-  const videoConstraints = {
-    video: {
-      width: { ideal: 1920, max: 7680 },
-      height: { ideal: 1080, max: 4320 },
-    },
-    audio: true,
-  };
+
 
   useEffect(() => {
     socketRef.current = io.connect(BASE_URL);
 
     navigator.mediaDevices
-      .getUserMedia(videoConstraints)
+      .getUserMedia(avSettings)
       .then((stream) => {
         userVideo.current.srcObject = stream;
         userStream.current = stream; //
@@ -200,12 +193,15 @@ const ClassroomPage = () => {
 
   const toggleMic = () => {
     const audioTrack = userStream.current
-      .getAudioTracks()
+      .getTracks()
       .find((track) => track.kind === 'audio');
     if (audioTrack.enabled) {
-      audioTrack.enabled = !audioTrack.enabled;
+      audioTrack.enabled = false;
+      console.log(audioTrack, 'audioTrack');
     } else {
       audioTrack.enabled = true;
+      console.log(audioTrack, 'audioTrack');
+
     }
     console.log(audioTrack.enabled, 'myMic');
   };
@@ -217,7 +213,6 @@ const ClassroomPage = () => {
             <div className="my-video">
               <video
                 playsInline
-                muted
                 ref={userVideo}
                 autoPlay
                 className="videoplayer-container"

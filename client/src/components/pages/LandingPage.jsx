c0bf8f67d5d47d96cbd5b22fff13c6e2097798f7
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-09 20:36:01 +0200

    feat: hero

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -4,6 +4,7 @@ import SignInPage from './SignInPage';
 import { scrollHandler } from '../../utils/scrollHandler';
 import Video from '../../assets/svg/video.svg';
 import Payment from '../../assets/svg/payment.svg';
+import Foam from '../../assets/svg/foam.svg';
 import Calendar from '../../assets/svg/calendar-compressed.svg';
 
 
@@ -25,11 +26,14 @@ const LandingPage = () => {
         </div>
     
         <div className="land l-hero" ref={heroRef}>
+          <div className="hero-box">
           <div className="box-topic" onMouseLeave={() => scrollHandler(mainRef)}>
             <h3 className="hero-title">Tools for Professionals</h3>
             <p className="hero-text">
               Mindfulness is a one-stop-shop for freelance Mindfulness, Wellness and Mental Health professionals.</p>
-              <p className="hero-text">Sign up and enter your own global studio</p>
+              <p className="hero-text">Start your own global studio today</p>
+              <SignInPage />
+          </div>
           </div>
         </div>
         
@@ -39,7 +43,7 @@ const LandingPage = () => {
               <div className="calendar_svg">
                 <img src={Calendar} alt="calendar"/>
               </div>
-              <h4>Create Events</h4>
+              <h4 className="box-title">Create Events</h4>
               <p className="description">Publish your therapy session, 1-on-1 coaching or private yoga class.
               Keep an agenda of your upcoming events and convert more, by allowing your clients to easily sign up.
               </p>
@@ -50,14 +54,14 @@ const LandingPage = () => {
                 <img src={Video} alt="calendar"/>
               </div>
               
-              <h4>Host Video Sessions</h4>
+              <h4 className="box-title">Host Video Sessions</h4>
               <p className="description">Create safe spaces for sharing. Mindfulness allows you to communicate directly to your client (p2p). We do not keep track of the content of your video calls, which means enhanced privacy for your communications.</p>
             </div>
             <div className="box-topic payment-box" onMouseLeave={() => scrollHandler(footerRef)}>
               <div className="payment_svg">
                 <img src={Payment} alt="calendar"/>
               </div>
-              <h4>Accept payments</h4>
+              <h4 className="box-title">Accept payments</h4>
               <p className="description">Provide a Payment Link or connect a Stripe account and immediately start collecting payments upon your clients sign up for your events.</p>
 
             </div>
